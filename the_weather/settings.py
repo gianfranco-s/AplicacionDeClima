d8e0884033126d5bedb5666dc7f72d40e95f3378
Gianfranco Salomone <gianfranco.s@gmail.com>
2021-08-04 19:10:07 -0300

    Agregue Procfile y requirements.txt

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=db_-of%wdkact)suvgdn!r!epg(3z80#!7mt+4b9dp_9&25wu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
